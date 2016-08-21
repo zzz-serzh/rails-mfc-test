778c7a57b899b2682c679883291516814a7f8b66
zzz_serzh <m4lab@mail.ru>
2016-08-21 16:35:51 +0300

    промежуточны коммит. Создан класс MfcOffice

---
+++ app/models/mfc_office.rb
@@ -0,0 +1,48 @@
+class MfcOffice
+
+  include DataMapper::Resource
+
+  property :id, Serial
+  #график работы.
+  #TODO: вынести в отдельный класс? Как эта штука заводится? уточнить
+  #property :schedule, String, :length => 128, :required => true
+
+  # категория: бэк или фронт. Может завести общий класс [Справочник]?
+  property :type, String, :length => 32, :required => true,  :format => "back|front"
+
+  #Регистрационный номер ЕСИА
+  property :esia, String, :length => 128, :required => true
+
+  #Реквизиты центра (офиса)
+  #Наименование офиса
+  property :name, String, :length => 256, :required => true
+  #Краткое наименование офиса
+  property :short_name, String, :length => 128, :required => true
+  #Директор
+  property :director, String, :length => 256, :required => true
+  #ОГРН
+  property :ogrn, String, :length => 13, :required => true, :format => "^[0-9]{13}$"
+  #КПП
+  property :kpp, String, :length => 9, :required => true, :format => "^[0-9]{9}$"
+  #ОКОГУ
+  property :kpp, String, :length => 7, :required => true, :format => "^[0-9]{7}$"
+  #okpo
+  property :kpp, String, :length => 8, :required => true, :format => "^[0-9]{8}$"
+
+  #Юридический адрес  
+  #has 1, :address, :required => true
+  belongs_to :jur_address, 'Address', :key => true
+
+  #Фактический адрес
+  #has 1, :address, :required => true
+  belongs_to :actual_address, 'Address', :key => true
+
+  #Телефон
+  property :phone, String, :length => 10, :required => true, :format => "^[0-9]{10}$"
+  #Сайт
+  property :site, String, :length => 256 
+  #Электронная почта
+  property :email, String, :format => :email_address
+
+
+end
