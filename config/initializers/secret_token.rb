160a7126e6244147bab39d32c33d4e894093c0b7
zzz_serzh <m4lab@mail.ru>
2016-08-21 04:14:36 -0400

    init commit. сгенерировано [rails new mfc -m http://datamapper.org/templates/rails.rb]

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mfc::Application.config.secret_token = '86e5cb5220050ae9adf5dd8d78c93f67a14fa92bb610184eebcf764e7abf37de98cb1f1aeecd0c372a8088dc49eeda15c68790523832243b0de1cf1ff76be79a'
