1f2ccb828f25dca9b07cfd45df1f5a2b34b026af
root <root@test118.morflot3>
2016-08-23 15:11:26 +0400

    Добавлены ссылки назад, изменена index страница

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Mfc::Application.routes.draw do
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
   resources :mfcoffices
-   #root 'welcome#index'
+  #root "welcome#index"
+  root :to => 'welcome#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
